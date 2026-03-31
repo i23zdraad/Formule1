84a4f53f0ed191edcbd1ecf321b6c0bfb52c9f71
i23zdraad <i23zdraad@oakm.cz>
2026-03-31 08:49:44 +0200

    skola 1

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+
+const connectDB = require("./config/db");
+
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
